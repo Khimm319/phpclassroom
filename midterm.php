95a96f30df49af460de8937b73ddd6f2be6cfd3b
Khimm319 <89382986+Khimm319@users.noreply.github.com>
2021-12-24 20:21:50 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -1,76 +1,85 @@
+
 <?php
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 
-  echo "<table border='1'>";
-  echo"<caption>";
-  echo "<h3>";
-  echo "<p>";
-  echo "รายงานสถานการโควิด-19 แยกจังหวัด";
-  echo "</p>";
-  echo "</h3>";
-  echo"</caption>";
-  echo "<tr align = 'center'>";
-  echo "<td bgcolor='#e5e5e5'>"; 
-  echo "ลำดับ";
-  echo "</td>"; 
-  echo "<td bgcolor='#56aaff'>"; 
-  echo "จังหวัด";
-  echo "</td>";
-  echo "<td bgcolor='#007fff'>"; 
-  echo "ผู้ป่วยใหม่";
-  echo "</td>"; 
-  echo "<td bgcolor='#00ffb2'>"; 
-  echo "ผู้ป่วยรวม";
-  echo "</td>"; 
-  echo "<td bgcolor='#00ff37'>"; 
-  echo "ผู้ป่วยใหม่ในประเทศ";
-  echo "</td>"; 
-  echo "<td bgcolor='#09bc30'>"; 
-  echo "ผู้ป่วยรวมในประเทศ";
-  echo "</td>"; 
-  echo "<td bgcolor='#FF6666'>"; 
-  echo "ผู้เสียชีวิตใหม่";
-  echo "</td>"; 
-  echo "<td bgcolor='#FF6633'>"; 
-  echo "ผู้เสียชีวิตรวม";
-  echo "</td>";
-  echo "<td bgcolor='#f7f7c5'>"; 
-  echo "วันที่อัพเดท";
-  echo "</td>";  
-  echo "</tr>";
 
-  foreach($data as $key=>$val){
+
+echo"<body>";
+echo"<center>";
+  
+    echo "<table border='1'>";
+    echo"<caption>";
+    echo "<h3>";
+    echo "<p>";
+    echo "รายงานสถานการโควิด-19 แยกจังหวัด";
+    echo "</p>";
+    echo "</h3>";
+    echo"</caption>";
+    echo "<tr align = 'center'>";
+    echo "<td bgcolor='#BEBEBE'>"; 
+    echo "ลำดับ";
+    echo "</td>"; 
+    echo "<td bgcolor='#FFFAFA'>"; 
+    echo "จังหวัด";
+    echo "</td>";
+    echo "<td bgcolor='#98FB98'>"; 
+    echo "ผู้ป่วยใหม่";
+    echo "</td>"; 
+    echo "<td bgcolor='##66CC00'>"; 
+    echo "ผู้ป่วยรวม";
+    echo "</td>"; 
+    echo "<td bgcolor='#FAF0E6'>"; 
+    echo "ผู้ป่วยใหม่ในประเทศ";
+    echo "</td>"; 
+    echo "<td bgcolor='#FAEBD7'>"; 
+    echo "ผู้ป่วยรวมในประเทศ";
+    echo "</td>"; 
+    echo "<td bgcolor='#FF6347'>"; 
+    echo "ผู้เสียชีวิตใหม่";
+    echo "</td>"; 
+    echo "<td bgcolor='#FF4500'>"; 
+    echo "ผู้เสียชีวิตรวม";
+    echo "</td>";
+    echo "<td bgcolor='#F0F8FF'>"; 
+    echo "วันที่อัพเดท";
+    echo "</td>";  
+    echo "</tr>";
+
+foreach($data as $key=>$val){
     echo "<tr align = 'center'>";
     echo "<tr align = 'center'>";
-    echo "<td bgcolor='#e5e5e5'>"; 
+    echo "<td bgcolor='#BEBEBE'>"; 
     echo $key+1;
     echo "</td>"; 
-    echo "<td bgcolor='#56aaff'>"; 
+    echo "<td bgcolor='#FFFAFA'>"; 
     echo $val->province;
     echo "</td>";
-    echo "<td bgcolor='#007fff'>"; 
+    echo "<td bgcolor='#98FB98'>"; 
     echo $val->new_case;
     echo "</td>";
-    echo "<td bgcolor='#00ffb2'>"; 
+    echo "<td bgcolor='##66CC00'>"; 
     echo $val->total_case;
     echo "</td>";
-    echo "<td bgcolor='#00ff37'>"; 
+    echo "<td bgcolor='#FAF0E6'>"; 
     echo $val->new_case_excludeabroad;
     echo "</td>";
-    echo "<td bgcolor='#09bc30'>"; 
+    echo "<td bgcolor='#FAEBD7'>"; 
     echo $val->total_case_excludeabroad;
     echo "</td>";
-    echo "<td bgcolor='#FF6666'>"; 
+    echo "<td bgcolor='#FF6347'>"; 
     echo $val->new_death;
     echo "</td>";
-    echo "<td bgcolor='#FF6633'>"; 
+    echo "<td bgcolor='#FF4500'>"; 
     echo $val->total_death;
     echo "</td>"; 
-    echo "<td bgcolor='#f7f7c5'>"; 
+    echo "<td bgcolor='#F0F8FF'>"; 
     echo $val->update_date;
     echo "</td>";
     echo "</tr>";
 
   }
   echo "</table>";
+
+echo"</center>";
+echo"</body>";
